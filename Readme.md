bd5dd79bb85fa5376311be54fd842b36ad2c4cf9
wadhwas <shailesh.wadhwa@hmhco.com>
2020-01-28 16:00:41 +0000

    test: test

---
+++ Readme.md
@@ -0,0 +1,28 @@
+## Protractor Tutorial
+
+Aim: To create test framwork using Protractor tool 
+### Steps to Initialize the project
+
+Step-1: Create project directory & Initialize as Node Project
+```
+> mkdir protractor-tutorial
+
+> npm init  // This creates package.json file
+
+> npm install protractor // This installs protractor for this project & gets all dependencies
+```
+
+Step-2: Get the Selenium Server running through `webdriver-manager` which is provided with protractor
+
+```
+> webdriver-manager update
+> webdriver-manager start  // starts the Selenium server
+```
+Note: If first command fails for geckodriver, inside `package.json`, add following to the `"scripts" ` :
+```
+"webdriver-update": "webdriver-manager update --gecko false"
+```
+
+Step-3: Now follow the tutorial at the link:
+
+https://github.com/angular/protractor/blob/master/docs/tutorial.md
---
+++ tests/spec.js
@@ -0,0 +1,42 @@
+describe('Protractor Demo App', function(){
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var gobutton = element(by.id('gobutton'));
+    var result = element(by.binding('latest'));
+    var history = element.all(by.repeater('result in memory'));
+
+    beforeEach(function(){
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+
+    it('should have a title', function(){    
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+    it('should add one and two', function(){
+        firstNumber.sendKeys(1);
+        secondNumber.sendKeys(2);
+        
+        gobutton.click();
+        
+        expect(result.getText()).toEqual('3');
+    });
+
+    it('should read the value from an input', function(){
+        firstNumber.sendKeys(20);
+        expect(firstNumber.getAttribute('value')).toEqual('20');
+    });
+
+    function add(a, b){
+        firstNumber.sendKeys(a);
+        secondNumber.sendKeys(b);
+        gobutton.click();
+    }
+
+    it('should have a history', function(){
+        add(1, 2);
+        add(2, 3);
+
+        expect(history.count()).toEqual(2);
+    });
+
+});
